22200d6f6167aa68524f4cf9a0deb2ee56a593b0
Konstantin Merenkov <56008710+Merkost@users.noreply.github.com>
2022-10-07 15:56:21 +1000

    Rotate matrix destroyed

--- 2/02U.py
+++ 2/02U.py
@@ -12,44 +12,52 @@
 """
 
 
-def PrintMatrix(array):
-    for i in range(len(array)):
-        print(*[array[i][j] for j in range(len(array[0]))])
+def rotate(arr):
+    matrix = []
+    for i in zip(*arr):
+        matrix.append(tuple(i))
+    return matrix
 
 
-matrix = []
+def printMatrix(arr):
+    for i in arr:
+        print(" ".join(list(map(str, i))))
+
+def printMatrixReversed(arr):
+    for i in arr:
+        print(" ".join(reversed(list(map(str, i)))))
+
+
 height, width, turn = map(int, input().split())
-if turn % 4 == 0:
-    for i in range(height):
-        print(input())
-elif turn % 4 == 1:
-    for i in range(height):
-        matrix.insert(0, list(input().split()))
-    PrintMatrix(list(zip(*matrix)))
-elif turn % 4 == 2:
-    for i in range(height):
-        matrix.insert(0, list(input().split())[::-1])
-    PrintMatrix(matrix)
-else:
-    for i in range(height):
-        matrix.append(list((input().split())))
-    PrintMatrix(list(zip(*matrix))[::-1])
+lst = [tuple(input().split()) for _ in range(height)]
+turn %= 4
+if turn == 0:
+    printMatrix(lst)
+elif turn == 1:
+    printMatrix(rotate(reversed(lst)))
+    # [print(*i) for i in zip(*reversed(lst))]
+elif turn == 2:
+    printMatrixReversed(reversed(lst))
+elif turn == 3:
+    printMatrix(reversed(rotate(lst)))
+    # [print(*i) for i in zip(*reversed(lst))]
 
-    # for i in range(height):
-    #     matrix.append(list(input().split()))
+    # [print(*zip(*[input().split()[::-1] for _ in range(height)]), sep = "\n")]
 
-    # if turn % 4 == 0:
-    #     PrintMatrixFaster(matrix)
-    # elif turn % 4 == 1:
-    #     PrintMatrixFaster(list(zip(*matrix[::-1])))
-    # elif turn % 4 == 2:
-    #     PrintMatrixFaster(list(map(lambda x: x[::-1], matrix[::-1])))
-    # elif turn % 4 == 3:
-    #     PrintMatrixFaster(list(zip(*matrix))[::-1])
+# def PrintMatrix(array):
+#     [print(*[array[i][j] for j in range(len(array[0]))]) for i in range(len(array))]
 
 
-
-
+# height, width, turn = map(int, input().split())
+# turn %= 4
+# if turn == 0:
+#     [print(input()) for i in range(height)]
+# elif turn == 1:
+#     [print(*i) for i in zip(*[input().split() for _ in range(height)][::-1])]
+# elif turn == 2:
+#     [print(*i) for i in [input().split()[::-1] for _ in range(height)])]
+# elif turn == 3:
+#     [print(*i) for i in zip(*[input().split()[::-1] for _ in range(height)])]
 
 """
 2 3 3
@@ -61,3 +69,20 @@ else:
 4 5 6
 7 8 9
 """
+
+# [[a, b] for a, b in zip(xrange(100), xrange(100))]
+
+# elif turn % 4 == 1
+# PrintMatrix3(tuple(zip(*[input().split()[::-1] for i in range(height)])))
+
+# for i in range(height):
+#     matrix.append(list(input().split()))
+
+# if turn % 4 == 0:
+#     PrintMatrixFaster(matrix)
+# elif turn % 4 == 1:
+#     PrintMatrixFaster(list(zip(*matrix[::-1])))
+# elif turn % 4 == 2:
+#     PrintMatrixFaster(list(map(lambda x: x[::-1], matrix[::-1])))
+# elif turn % 4 == 3:
+#     PrintMatrixFaster(list(zip(*matrix))[::-1])
