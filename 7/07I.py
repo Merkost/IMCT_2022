72e313e7a5a5e5b57cd29e707948cf85abdc71f4
Konstantin Merenkov <56008710+Merkost@users.noreply.github.com>
2022-11-17 13:58:20 +1000

    7 I almost

--- 7/07I.py
+++ 7/07I.py
@@ -10,13 +10,14 @@ def nearest(points: np.ndarray, a: np.ndarray) -> np.ndarray:
         a: A 1-dimensional np.ndarray, `a`.shape[0] == `points`.shape[1]
 
     '''
-    for point in points:
-        print(point - np.array([9,9]))
     # print([[f"{a}, {b}" for a, b in zip(np.array(point), a.repeat(points.size))] for point in points])
-    distances = np.array(np.sqrt(np.sum(pow(a - point, 2))) for point in points)
+    distances = np.array([np.sqrt(np.sum(np.power(a - point, 2))) for point in points])
     # distances = np.array(np.sqrt(sum(pow(a - b, 2) for a, b in [zip(point, a) for point in points])))
     # distance = np.sqrt(sum(pow(a - b, 2) for a, b in zip(point_1, point_2)))
 
-    return distances
+    # Find index of minimum value from 2D numpy array
+    result = np.where(distances == np.min(distances))
+    return points[result].flatten()
+
 
 print(*nearest(np.array([[0, 0], [3, 3]]), np.array([1, 1])))
